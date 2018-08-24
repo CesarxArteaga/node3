075543eb8af735653537bfb5549c28e7d2a69939
CesarxArteaga <ctmces94@gmail.com>
2018-08-24 10:02:55 -0500

    jajsj

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000 ;
+
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', 
+           exphbs({defaultLayout: 'main'}));
+
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => res.render('home'));
+
+//login route
+app.get('/login', (req, res) => res.render('login', {csrf: 'abc'}));
+
+//body.parse
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.post('/process', function(req,res){
+  console.log('formulario:'+ req.query.form);
+  console.log('nombre'+ req.body.name);
+  console.log('nombre'+ req.body.email);
+});
+
+
+var fortune = require('./lib/fortune');
+
+//usa modulos de la libreria fortune.js
+app.get('/about', (req, res) => 
+          res.render('about', {fortune: fortune.getFortune() })
+       );
+
+//archivos estáticos
+app.use(express.static(path.join(__dirname,'/public')));
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
